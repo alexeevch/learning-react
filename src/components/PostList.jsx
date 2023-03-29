7fe11ef3a9abd2fa464ce73fc0e3570d662f9a9a
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-03-29 13:58:12 +0300

    Вынес список постов в отдельный компонент PostList

---
+++ src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostItem from "./PostItem";
+
+const PostList = ({posts}) => {
+    return (
+        <div>
+            <h1 className="title" style={{textAlign: "center"}}>Список постов</h1>
+            {posts.map(post =>
+                <PostItem key={post.id} post={post}/>
+            )}
+        </div>
+    );
+};
+
+export default PostList;

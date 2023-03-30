c168d3fc9d78de6b6672bd2f6dfa650375d86949
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-03-30 10:59:40 +0300

    Рефакторинг PostItem

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const PostItem = (props) => {
+const PostItem = ({post}) => {
     return (
         <div className="post">
             <div className="post__content">
                 <div className="post__title">
-                    {props.post.id}. {props.post.title}
+                    {post.id}. {post.title}
                 </div>
                 <div className="post__body">
-                    {props.post.body}
+                    {post.body}
                 </div>
             </div>
             <div className="post__btns">

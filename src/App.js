efe349ca485c2d6b7a9f60674e7034a8674c7edb
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-03-30 13:15:58 +0300

    Деструктуризация, добавил компонент PostForm

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
 import React, {useState} from 'react';
 import '../src/styles/App.css'
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'JavaScript', body: "JS - Язык програмирования"},
-        {id: 2, title: 'JavaScript 2', body: "JS - Язык програмирования 2"},
-        {id: 3, title: 'JavaScript 3', body: "JS - Язык програмирования 3"},
+        {id: 1, title: 'JavaScript', body: 'JS - Язык програмирования'},
+        {id: 2, title: 'JavaScript 2', body: 'JS - Язык програмирования 2'},
+        {id: 3, title: 'JavaScript 3', body: 'JS - Язык програмирования 3'},
     ]);
 
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-
-
-    const addNewPost = (e) => {
-        e.preventDefault();
-        
-        const newPost = {
-            id: Date.now(),
-            title,
-            body,
-        }
-
+    const createPost = (newPost) => {
         setPosts([...posts, newPost]);
-        setTitle('');
-        setBody('');
     }
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    placeholder="Заголовок"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                />
-                <MyInput
-                    placeholder="Описание"
-                    value={body}
-                    onChange={e => setBody(e.target.value)}
-                />
-                <MyButton onClick={addNewPost}>Создать</MyButton>
-            </form>
+            <PostForm create={createPost}/>
             <PostList posts={posts} title="Список постов"/>
         </div>
     );
